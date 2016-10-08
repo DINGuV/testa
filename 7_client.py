a694075da6fe727da6c1ffa49ee7af96da98a8a4
丁超文 <kobeding@dingchawendeMBP.lan>
2016-10-08 12:59:08 +0800

    add 7

---
+++ 7_client.py
@@ -0,0 +1,15 @@
+#客户控制端：利用socket模块实现数据的发送和接收
+
+import socket
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect(('127.0.0.1',9999))
+print(s.recv(1024).decode('utf-8'))
+
+while True:
+    com = input(str('~####>'))
+    s.send(com.encode('utf-8'))
+    if com == 'exit':
+        break
+    data = s.recv(4096)
+    print(data.decode('utf-8'))
